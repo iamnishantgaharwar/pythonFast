31ff757e2611d462cbc3354409269d0d7ab90365
निशांत गहरवार <62183023+iamnishantgaharwar@users.noreply.github.com>
2025-05-19 12:50:13 +0530

    Revoke Secret Key

--- blog/database.py
+++ blog/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'postgresql://neondb_owner:7oGuihjlS6sD@ep-dawn-haze-a1s136dd-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require'
+SQLALCHAMY_DATABASE_URL = 'key'
 
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
